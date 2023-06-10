ab7637ea3a760dc1044a9674a3465f251c98fa27
Saurabh Singh <boyslike2play@gmail.com>
2023-06-10 14:39:24 +0530

    video play in production house card issue fixed

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -47,18 +47,20 @@ const ProductionHouse = () => {
     },
   ];
   return (
-    <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16">
-      {list.map((item, index) => (
-        <div
-          key={index}
-          className="border-[2px] border-gray-600 rounded-lg hover:scale-110 
-          transition-all duration-300 ease-in-out cursor-pointer relative shadow-xl shadow-black"
-        >
-         <video src={item.video} autoPlay loop playsInline className="absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50"/>
-          <img src={item.image} className=" w-full z-[1]" />
-         
-        </div>
-      ))}
+    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 '>
+        {list.map((item)=>(
+            <div key={item} className='border-[2px] border-gray-600
+            rounded-lg hover:scale-110 transition-all duration-300
+            ease-in-out cursor-pointer relative shadow-xl 
+            shadow-black
+            '>
+                 <video src={item.video} autoPlay loop playsInline muted 
+            className='absolute z-0  top-0 rounded-md 
+            opacity-0 hover:opacity-50'/> 
+                <img src={item.image} className='w-full z-[1] opacity-100' /> 
+               
+            </div>
+        ))}
     </div>
   );
 };
